28fcc846d52343047c4e2acaefc9414b91f56dd1
Amit Levy <levya@marykate.cs.washington.edu>
2009-11-03 11:27:19 -0800

    OneTimeValue Experiment

--- src/expt/edu/washington/cs/activedht/expt/OneTimeValue.java
+++ src/expt/edu/washington/cs/activedht/expt/OneTimeValue.java
@@ -5,6 +5,10 @@ import java.io.File;
 import java.io.PrintStream;
 import java.util.concurrent.Semaphore;
 
+import org.gudy.azureus2.core3.util.Timer;
+import org.gudy.azureus2.core3.util.TimerEvent;
+import org.gudy.azureus2.core3.util.TimerEventPerformer;
+
 import com.aelitis.azureus.core.dht.DHTOperationAdapter;
 import com.aelitis.azureus.core.dht.transport.DHTTransportContact;
 import com.aelitis.azureus.core.dht.transport.DHTTransportValue;
@@ -27,30 +31,61 @@ public class OneTimeValue {
 		final int data[] = new int[iterations];
 
 		String baseKey = "" + System.currentTimeMillis();
-		Thread.sleep(60000);
-
-		final Semaphore semaphore = new Semaphore(iterations);
-		semaphore.drainPermits();
+		Thread.sleep(1200000);
+		//final Semaphore semaphore = new Semaphore(iterations);
+		final Semaphore semas[] = new Semaphore[iterations];
 		for (int i = 0; i < iterations; ++i) {
-
-			byte[] key = (baseKey + i).getBytes();
+			final int _i = i;
+			final byte[] key = (baseKey + i).getBytes();
+			semas[i] = new Semaphore(1);
+			semas[i].acquire();
+			
 			peer.put(key, getOneTimeValueBytes("world"),
 					new DHTOperationAdapter() {
 						@Override
 						public void complete(boolean timeout) {
-							semaphore.release();
+							semas[_i].release();
 							System.out.println("Released");
 						}
 					});
-			Thread.sleep(0);
+
+			semas[_i].acquire();
+			long eventTime = System.currentTimeMillis() + 300000;
+			Timer t = new Timer(new String(key));
+			t.addEvent(eventTime, new TimerEventPerformer() {
+				public void perform(TimerEvent event) {
+					peer.get(key, new DHTOperationAdapter() {
+						boolean found = false;
+						public void read(DHTTransportContact contact,
+								DHTTransportValue value) {
+							found = true;
+							out.println(contact.getAddress().getHostName());
+						}
+						public void complete(boolean timeout) {
+							if (found) data[_i]++;
+						}
+					});
+				}
+			});
+			t.addEvent(eventTime + gap, new TimerEventPerformer() {
+				public void perform(TimerEvent event) {
+					peer.get(key, new DHTOperationAdapter() {
+						boolean found = false;
+						public void read(DHTTransportContact contact,
+								DHTTransportValue value) {
+							found = true;
+							out.println(contact.getAddress().getHostName());
+						}
+						public void complete(boolean timeout) {
+							if (found) data[_i]++;
+							semas[_i].release();
+						}
+					});
+				}
+			});
 		}
-		System.out.println("Waiting for puts to finish...");
-		semaphore.acquire(iterations);
-		semaphore.release(iterations * 2);
-		System.out.println(semaphore.availablePermits());
 		
-		
-		for (int i = 0; i < iterations; ++i) {
+		/*for (int i = 0; i < iterations; ++i) {
 			final int _i = i;
 			byte[] key = (baseKey + i).getBytes();
 			semaphore.acquire();
@@ -82,7 +117,10 @@ public class OneTimeValue {
 				}
 			});
 		}
-		semaphore.acquire(iterations * 2);
+		semaphore.acquire(iterations * 2);*/
+		for (Semaphore s : semas) {
+			s.acquire();
+		}
 		return data;
 	}
 
@@ -102,10 +140,10 @@ public class OneTimeValue {
 	}
 
 	public static void main(String[] args) throws Exception {
-		ActivePeer peer = new ActivePeer(Integer.parseInt(args[0]), args[1], true);
+		ActivePeer peer = new ActivePeer(Integer.parseInt(args[0]), args[1], false);
 		peer.init();
 		int data[] = new OneTimeValue(peer, new PrintStream(new File(
-				"/tmp/activedht/onetime.out"))).run(10000, 50);
+				"/tmp/activedht/onetime.out"))).run(20000, 500);
 		PrintStream out = new PrintStream(new File(
 				"/tmp/activedht/onetime.results"));
 		for (int d : data) {
@@ -113,6 +151,7 @@ public class OneTimeValue {
 		}
 		peer.stop();
 		System.out.println("DONE");
+		System.exit(0);
 	}
 
 }
