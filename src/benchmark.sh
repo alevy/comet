9b21ee4edd042a446980ddf1d0763373444c4ec8
Amit Levy <levya@marykate.cs.washington.edu>
2010-02-10 17:39:02 -0800

    Fix path in benchmark.sh

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -3,7 +3,7 @@
 for i in $(seq 10 10 100)
 do
  echo "Benchmarking $i"
- ssh nethack.cs.washington.edu "screen -d -m workspace/activedht/src/runclass edu.washington.cs.activedht.expt.ActivePeer -h nethack.cs.washington.edu -p 4321 -b nethack.cs.washington.edu:4321"
+ ssh nethack.cs.washington.edu "screen -d -m ~/workspace/activedht/src/runclass.sh edu.washington.cs.activedht.expt.ActivePeer -h nethack.cs.washington.edu -p 4321 -b nethack.cs.washington.edu:4321"
  `dirname $0`/runclass.sh edu.washington.cs.activedht.expt.LuaMicrobenchmark -n $i -h `hostname -f` -p 1234 -b nethack.cs.washington.edu:4321 $*
  ssh nethack.cs.washington.edu "killall java"
 done
